2885a98acc14b8aef472695defe6e323c866f8dd
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-06-21 21:43:04 -0400

    Update: Refactored while loop Extracted 3 methods to make while loop more readable

--- JavaScript/Basics/todoListScript.js
+++ JavaScript/Basics/todoListScript.js
@@ -3,23 +3,13 @@ var todos = ["Win Lottery"];
 var input = prompt("What would you like to do?");
 while(input !=="quit"){
     if(input === "new"){
-        var newInput = prompt("Enter a new task todo");
-        todos.push(newInput);
-        console.log(todos);
+        createNewTodo();
     }
     else if(input === "view"){
-        //alert(todos);
-        console.log("*****");
-        todos.forEach(function(todo, index){
-            console.log(index+": "+todo);
-        });
-        console.log("*****");
+        viewTodos();
     }
     else if(input ==="delete"){
-        var index = prompt("Enter index of todo to delete");
-        //Delete that todo via splice()
-        todos.splice(index,1);
-        console.log("Deleted Todo");
+        deleteTodo();
     }
     else{
         alert("Please enter use one of the commands\nnew, view, quit");
@@ -29,4 +19,25 @@ while(input !=="quit"){
 }
 alert("Application Closed");
 console.log("Application Closed");
+//Push new todo to end of array
+function createNewTodo(){
+    var newInput = prompt("Enter a new task todo");
+    todos.push(newInput);
+    console.log(todos);
+}
+//View all todos
+function viewTodos(){
+    console.log("*****");
+    todos.forEach(function(todo, index){
+        console.log(index+": "+todo);
+    });
+    console.log("*****");
+}
+//Delete todo at given index
+function deleteTodo(){
+    var index = prompt("Enter index of todo to delete");
+        //Delete that todo via splice()
+        todos.splice(index,1);
+        console.log("Deleted Todo");
+}
 
