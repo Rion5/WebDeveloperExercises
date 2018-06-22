a8623155aea7269715f1f6f6c6c895aef709872e
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-06-22 16:09:23 -0400

    Add: Defining DOM exercise 1st exercise where I am using JavaScript to target HTML elements via the DOM

---
+++ JavaScript/DOM/definingDOMScript.js
@@ -0,0 +1,16 @@
+//SELECT
+var h4 = document.querySelector("h4")
+h4.style.color = "pink";
+var isBlue = false;
+
+//MANIPULATE
+setInterval(function(){
+    if(isBlue){
+        h4.style.background = "white";
+        isBlue = false;
+    }
+    else{
+        h4.style.background = "blue";
+        isBlue = true;
+    }
+}, 1000);
