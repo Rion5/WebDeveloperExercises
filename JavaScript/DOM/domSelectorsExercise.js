07b0b5d918085e1979a988e7a02d9f41a57f38a4
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-06-26 16:18:18 -0400

    Add: Practice Problems 3 & 4 3) Using js, edit an image element 4) Using js for-loop, edit all anchor tags

--- JavaScript/DOM/domSelectorsExercise.js
+++ JavaScript/DOM/domSelectorsExercise.js
@@ -25,3 +25,24 @@ document.querySelector("p");
 var h1Tag = document.querySelector("h1");
 h1Tag.classList.add("special");                 //Adds the class 'special' to first h1 tag
 h1Tag.classList.toggle("special");              //Toggles the special class: OFF
+
+//3) Playing with Google Image
+// Using just JavaScript, change the Google Logo to another Image
+var logo = document.querySelector("#hplogo");
+logo.setAttribute("srcset","http://greenfrogestatesales.com/wp-content/uploads/2018/02/facebook-logo.png");
+logo.style.width = "500px";
+logo.style.padding = "15px";    //padding - the space betwen the content and its borders
+logo.style.border = "3px solid darkblue";
+
+//4) Use a For-Loop to loop through each element within the links
+var links = document.getElementsByTagName("a");
+for(var i = 0; i< links.length; i++){
+    console.log(links[i].textContent);
+    console.log(links[i]);
+    links[i].style.background ="pink";
+}
+
+//In this example, I am looping through all links, and setting the ref to amazon.com
+for(var i = 0; i < links.length; i++){
+    links[i].setAttribute("href","https://amazon.com");
+} 
