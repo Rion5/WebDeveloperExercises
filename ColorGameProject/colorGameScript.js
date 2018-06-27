78ca39365387678a71086d81f202d0d46bf98966
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-06-27 16:03:36 -0400

    Add: squares click event. If the clicked color is incorrect to the pickedColor, hide the square

--- ColorGameProject/colorGameScript.js
+++ ColorGameProject/colorGameScript.js
@@ -7,15 +7,31 @@ var colors = [
     "rgb(0, 0, 255)",
     "rgb(255, 0, 255)",
 ];
-
-//Square Elements
-var squares = document.querySelectorAll(".square");
-
+//Display the pickedColor
+//Hard Coded pickedColor (later on will randomize)
 var pickedColor = colors[3];
-
 var colorDisplay = document.getElementById("colorDisplay");
 colorDisplay.textContent = pickedColor;
-//Loop though each square and assign it a color from the colors array
+//Square Elements
+var squares = document.querySelectorAll(".square");
+//Display Message (Correct or Try Again)
+var messageDisplay = document.querySelector("#message");
+
+//Loop though each square 
+//      assign it a color from the colors array
+//      add a click event listener
 for(var i = 0; i<squares.length; i++){
     squares[i].style.backgroundColor = colors[i];
+    squares[i].addEventListener("click", function(){
+        var clickedColor = this.style.backgroundColor;
+        if(clickedColor === pickedColor){
+            messageDisplay.textContent = "Correct!";
+        }
+        else{
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
+        }
+    });
 }
+
+
