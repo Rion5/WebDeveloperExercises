644f16e2f2016ec3f6ad5ee55ceb1eb08eb09b61
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-06-27 13:38:20 -0400

    Add: Game Over Functionality. Max score of 5

--- JavaScript/DomAdvanced/domClickEventPt2.js
+++ JavaScript/DomAdvanced/domClickEventPt2.js
@@ -7,26 +7,40 @@ var p1Score = 0;
 var p2Button = document.getElementById("p2");
 var p2Display = document.querySelector("#p2ScoreDisplay");
 var p2Score = 0;
+//If winningScore is reached, change gameOver to true
+var winningScore = 5;
+var gameOver = false;
 
 //Reset Button
 var resetButton = document.getElementById("reset");
 
 //P1 Button Click
 p1Button.addEventListener("click",function(){
-    p1Score++;
-    p1Display.textContent = p1Score;
-    console.log("Player 1: ", p1Score);
+    if(!gameOver){
+        p1Score++;
+        p1Display.textContent = p1Score;
+        if(p1Score === winningScore){
+            gameOver = true;
+        }
+        console.log("Player 1: ", p1Score);
+    }
 });
 
 //P2 Button Click
 p2Button.addEventListener("click",function(){
-    p2Score++;
-    p2Display.textContent = p2Score;
-    console.log("Player 2: ", p2Score);
+    if(!gameOver){
+        p2Score++;
+        p2Display.textContent = p2Score;
+        if(p2Score === winningScore){
+            gameOver = true;
+        }
+        console.log("Player 2: ", p2Score);
+    }
 });
 
 //Reset Button Click
 resetButton.addEventListener("click",function(){
+    gameOver = false;
     p1Score = p2Score = 0;
     p1Display.textContent = p2Display.textContent = p1Score;
     console.log("Score reset to 0");
