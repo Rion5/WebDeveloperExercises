349a78d50f70978c31ab3ee192c63d1c315a56e8
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-06-27 14:47:07 -0400

    Add: Toggle winner class if gameOver Add: Input Box to determine max score using eventListener change

--- JavaScript/DomAdvanced/domClickEventPt2.js
+++ JavaScript/DomAdvanced/domClickEventPt2.js
@@ -7,7 +7,10 @@ var p1Score = 0;
 var p2Button = document.getElementById("p2");
 var p2Display = document.querySelector("#p2ScoreDisplay");
 var p2Score = 0;
+//Input Box
+var numInput = document.querySelector("input[type='number']");
 //If winningScore is reached, change gameOver to true
+var winningScoreDisplay = document.querySelector("p span");     //display = the <span> element within the <p>
 var winningScore = 5;
 var gameOver = false;
 
@@ -20,6 +23,7 @@ p1Button.addEventListener("click",function(){
         p1Score++;
         p1Display.textContent = p1Score;
         if(p1Score === winningScore){
+            p1Display.classList.add("winner");
             gameOver = true;
         }
         console.log("Player 1: ", p1Score);
@@ -32,6 +36,7 @@ p2Button.addEventListener("click",function(){
         p2Score++;
         p2Display.textContent = p2Score;
         if(p2Score === winningScore){
+            p2Display.classList.add("winner");
             gameOver = true;
         }
         console.log("Player 2: ", p2Score);
@@ -43,5 +48,14 @@ resetButton.addEventListener("click",function(){
     gameOver = false;
     p1Score = p2Score = 0;
     p1Display.textContent = p2Display.textContent = p1Score;
+    //Selecting whichever element has the winner class to it, and toggling it off
+    var winner = document.querySelector(".winner");
+    winner.classList.remove("winner");
     console.log("Score reset to 0");
+});
+
+//Input Box - Change Event
+numInput.addEventListener("change", function(){
+    winningScore = (Number)(numInput.value);
+    winningScoreDisplay.textContent = numInput.value;
 });
