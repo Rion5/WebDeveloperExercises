26db99b09a644ff43e805cee8c8e4c4cebb3212a
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-06-27 12:11:19 -0400

    Add Functionality: Player 1, Player 2, and Reset Button working as intended

--- JavaScript/DomAdvanced/domClickEventPt2.js
+++ JavaScript/DomAdvanced/domClickEventPt2.js
@@ -1,17 +1,33 @@
 //Using 2 different ways to select the button
 //Player 1 Button
 var p1Button = document.querySelector("#p1");
+var p1Display = document.getElementById("p1ScoreDisplay");
 var p1Score = 0;
 //Player 2
 var p2Button = document.getElementById("p2");
+var p2Display = document.querySelector("#p2ScoreDisplay");
 var p2Score = 0;
 
+//Reset Button
+var resetButton = document.getElementById("reset");
+
+//P1 Button Click
 p1Button.addEventListener("click",function(){
     p1Score++;
-    console.log(p1Score);
+    p1Display.textContent = p1Score;
+    console.log("Player 1: ", p1Score);
 });
 
+//P2 Button Click
 p2Button.addEventListener("click",function(){
     p2Score++;
-    console.log(p2Score);
+    p2Display.textContent = p2Score;
+    console.log("Player 2: ", p2Score);
+});
+
+//Reset Button Click
+resetButton.addEventListener("click",function(){
+    p1Score = p2Score = 0;
+    p1Display.textContent = p2Display.textContent = p1Score;
+    console.log("Score reset to 0");
 });
