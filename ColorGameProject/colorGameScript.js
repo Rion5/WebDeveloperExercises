dc31d86db0987ca964e0fbb90b5f583044712613
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-06-27 15:43:27 -0400

    Add: JavaScript Code to loop through each square and add color to it. In future update, I will randomize it.

--- ColorGameProject/colorGameScript.js
+++ ColorGameProject/colorGameScript.js
@@ -1,1 +1,21 @@
-console.log("connected");
+//List of Colors
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)",
+];
+
+//Square Elements
+var squares = document.querySelectorAll(".square");
+
+var pickedColor = colors[3];
+
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor;
+//Loop though each square and assign it a color from the colors array
+for(var i = 0; i<squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+}
