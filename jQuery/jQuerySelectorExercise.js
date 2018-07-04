7dcd1e882008aa822b05d978ef81f4edb138ca5d
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-07-04 18:51:56 -0400

    Added: Completed Exercise

--- jQuery/jQuerySelectorExercise.js
+++ jQuery/jQuerySelectorExercise.js
@@ -1,3 +1,16 @@
+//1)
 if(jQuery){
     console.log("jQuery Loaded");
 }
+
+//2) Select all divs and give them a purple background
+$("div").css("background","purple");
+
+//3)Select divs with the class highlight and make them 200px wide
+$(".highlight").css("width","200px");
+
+//4) Select the div with id "third" and give it an orange border
+$("#third").css("border","4px solid orange");
+
+//5) Bonus: Select the first div only and change its font color to pink
+$("div:first-of-type").css("color","pink");
