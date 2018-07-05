d25d7316b275ab764505cbe2e10665fc1c7386e1
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-07-05 15:53:15 -0400

    Add: event.stopPropagation Clicking span fades list element, and then removes the element

--- ToDoListProject/assets/js/toDoList.js
+++ ToDoListProject/assets/js/toDoList.js
@@ -2,3 +2,11 @@
 $("li").click(function () {
     $(this).toggleClass("completed");
 });
+
+//Click on the X to delete task
+$("span").click(function(event){
+    $(this).parent().fadeOut(1000,function(){
+        $(this).remove();
+    });
+    event.stopPropagation(); //this will stop the event from bubbling up to parent elements
+});
