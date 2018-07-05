33802c1d7665bd5c9fd42e072fd7f043a8324a51
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-07-05 11:36:15 -0400

    Add: New example and update comments

--- jQuery/jQueryEventsScript.js
+++ jQuery/jQueryEventsScript.js
@@ -1,13 +1,28 @@
-//click()
-//jQuery's click() method is a quick and easy way to add a click listener to elements
+/**Objectives
+ * click()
+    - jQuery's click() method is a quick and easy way to add a click listener to elements
+ 
+ * keypress()
+ * on()
+ */
 
+// click()
 //prints when item with id 'submit' is clicked
 $("#submit").click(function(){
-    console.log("Another Click");
+    console.log("Submit Click");
 });
 
 //alerts when ANY button is clicked
 $("button").click(function(){
-    alert("Someone clicked a button");
+    console.log("Someone clicked a button");
 });
 
+$("h1").click(function(){
+    alert("h1 clicked");
+});
+
+$("button").click(function(){
+    var btnText = $(this).text();
+    console.log("you clicked " + btnText);
+    $(this).css("background","pink");
+});
