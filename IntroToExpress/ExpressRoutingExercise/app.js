fb294b22f0d6b3e6ce3fe8193f3cbd86a465ea2d
Rion5 <chaigrindean@gmail.com>
2018-07-19 14:00:19 -0400

    Added: Express framework 'npm install express --save'

---
+++ IntroToExpress/ExpressRoutingExercise/app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+/** Instructions
+Visiting "/" should print "Hi, welcome to my page."
+==========================================================================
+Visiting "/speak/pig" should print "The pig says 'Oink'"
+Visiting "/speak/cow" should print "The cow says 'Moo'"
+Visiting "/speak/dog" should print "the dog says 'Bow Wow'"
+==========================================================================
+Visiting "/repeat/hello/3" should print "hello hello hello"
+Visiting "/repeat/hello/5" should print "hello hello hello hello hello"
+Visiting "/repeat/blah/2"  should print "blah blah"
+ */
