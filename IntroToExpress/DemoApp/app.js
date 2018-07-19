d5afde02241bf864062755a6f99adda70b230151
Rion5 <chaigrindean@gmail.com>
2018-07-19 11:35:22 -0400

    Added: Request / Respond example

--- IntroToExpress/DemoApp/app.js
+++ IntroToExpress/DemoApp/app.js
@@ -1,5 +1,28 @@
-console.log("Demo Express App");
 //Include Express
-var expess = require("express");
+var express = require("express");
 //Execute and save into variable called 'app'
 var app = express();
+
+//"route-path" => "Response"
+// "/" => "Hello"
+app.get("/", function(req, res){
+    res.send("Hello");
+});
+
+// "/bye" => Good Bye
+app.get("/bye", function(req, res){
+    res.send("Good Bye");
+}) ;
+
+// "/contact" => Contact Page
+app.get("/contact", function(req, res){
+    res.send("Contact Page");
+});
+
+
+//Tell Express to listen for requests (start server)
+//Note: process.env.PORT to use in c9
+app.listen(5500, function(){
+    console.log("Server has Started");
+    console.log("Go to 'localhost:5500'");
+});
