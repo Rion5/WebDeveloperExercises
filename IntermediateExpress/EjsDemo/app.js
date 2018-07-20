8e63e6ce70f887b2d9bcc93e87785c0f9c82d5e2
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-07-19 22:01:55 -0400

    Added: Example of EJS Conditional

--- IntermediateExpress/EjsDemo/app.js
+++ IntermediateExpress/EjsDemo/app.js
@@ -4,17 +4,22 @@ var app = express();
 //Home
 app.get("/",function(req,res){
     res.render("home.ejs");
-    //res.send("<h1>Welcome to the Home Page!</h1><h2>Chai Grindean</h2>");
 })
 
 
 app.get("/love/:thing", function(req,res){
     var thing = req.params.thing;
-    //res.send("You love "+ thing);
     res.render("love.ejs", {thingInput: thing});
 });
 
-
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post 1", author: "John"},
+        {title: "Post 2", author: "Doe"},
+        {title: "Post 3", author: "Joe"}
+    ];
+    res.render("posts.ejs", {posts: posts});
+});
 //Start Server
 app.listen(5500,function(req,res){
     console.log("Server Started!");
