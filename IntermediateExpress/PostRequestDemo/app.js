19f20cf32a1d8fa9c87efd8f4329c885bfe01c53
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-07-22 10:41:43 -0400

    Added: app.post, using body parser to get the input from the textbox

--- IntermediateExpress/PostRequestDemo/app.js
+++ IntermediateExpress/PostRequestDemo/app.js
@@ -1,19 +1,25 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var friends = ["Tony", "Matt", "David"];
 
 app.get("/",function(req,res){
     res.render("home");
 });
 
 app.get("/friends", function(req,res){
-    var friends = ["Tony", "Matt", "David"];
-
     res.render("friends", {friends: friends});
 });
 
-
+app.post("/addFriend", function(req, res){
+    var newFriend = req.body.newFriendName;
+    console.log(req.body.newFriendName);
+    friends.push(newFriend);
+    res.redirect("/friends");
+});
 
 
 
