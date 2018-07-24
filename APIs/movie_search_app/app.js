9afa0d6ef4a938f0ad950fab7ff494e00d2ebd2e
Rion5 <chaigrindean@gmail.com>
2018-07-24 11:05:20 -0400

    Added: search.ejs template Enables user to type a title to search the OMDb

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -5,15 +5,18 @@ var app = express();
 app.set("view engine","ejs");
 //Home
 app.get("/",function(req,res){
-    res.send("Home Page");
+    res.render("search");
 });
 //Results
 app.get("/results",function(req,res){
+    //Getting query from the input on the home page and build url
+    var query = req.query.searchQuery;
+    var url = "http://www.omdbapi.com/?s="+query+"&apikey=thewdb";
     //1st - Send a request,
     //2nd - Check for errors
     //3rd - Parse the html body string into JSON
     //4th - Res.send desired result
-    request("http://www.omdbapi.com/?s=star&apikey=thewdb",function(error,response,body){
+    request(url,function(error,response,body){
         if(!error && response.statusCode == 200){
             var parsedData = JSON.parse(body);
             //res.send(results["Search"][0]["Title"]);
