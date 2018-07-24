1f1d0421f096893e79d5b620a9f671e98b8adc59
Rion5 <chaigrindean@gmail.com>
2018-07-24 10:11:55 -0400

    Added: app.js and npm packages Example of how to send an API request, parse data into JSON, and display results

---
+++ APIs/movie_search_app/app.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var request = require("request");
+var app = express();
+
+//Home
+app.get("/",function(req,res){
+    res.send("Home Page");
+});
+//Results
+app.get("/results",function(req,res){
+    //1st - Send a request,
+    //2nd - Check for errors
+    //3rd - Parse the html body string into JSON
+    //4th - Res.send desired result
+    request("http://www.omdbapi.com/?s=star&apikey=thewdb",function(error,response,body){
+        if(!error && response.statusCode == 200){
+            var results = JSON.parse(body);
+            res.send(results["Search"][0]["Title"]);
+        }
+    });
+});
+//Request Failed
+app.get("/*",function(req,res){
+    res.send("Error: Request failed");
+});
+
+
+
+//Start Server
+app.listen(5500, function(){
+    console.log("Server has Started");
+    console.log("Go to 'localhost:5500'");
+});
