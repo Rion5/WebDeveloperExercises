98adf69d5b410faf5549b4eeb736558bb75a81d8
Rion5 <chaigrindean@gmail.com>
2018-08-02 16:35:15 -0400

    Added mongoDB Schema and Model

--- RESTfulRouting/RESTfulBlogApp/app.js
+++ RESTfulRouting/RESTfulBlogApp/app.js
@@ -8,9 +8,16 @@ app.set("view engine", "ejs");
 app.use(express.static("public")); // Use the following code to serve images, CSS files, and JavaScript files in a directory named public
 app.use(bodyParser, urlencoded({extended: true}));
 
+//MongoDB Schema
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
 
-
-
+//Compile into Model
+var Blog = mongoose.model("Blog",blogSchema);
 
 //Start Server
 app.listen(5500,function(){
