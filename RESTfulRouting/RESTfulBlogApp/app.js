3bf42defde321dcd7793d8089d629b0514e5aa70
Rion5 <chaigrindean@gmail.com>
2018-08-02 16:51:44 -0400

    Added: index.ejs Following RESTful, this sends a GET request to display all blogs

--- RESTfulRouting/RESTfulBlogApp/app.js
+++ RESTfulRouting/RESTfulBlogApp/app.js
@@ -2,11 +2,11 @@ var express     = require("express"),
     mongoose    = require("mongoose"),
     bodyParser  = require("body-parser"),
     app         = express();
-mongoose.connect("mongodb://localhost:27017/restful_blog_app");
+mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser: true});
 
 app.set("view engine", "ejs");
 app.use(express.static("public")); // Use the following code to serve images, CSS files, and JavaScript files in a directory named public
-app.use(bodyParser, urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 //MongoDB Schema
 var blogSchema = new mongoose.Schema({
@@ -18,6 +18,18 @@ var blogSchema = new mongoose.Schema({
 
 //Compile into Model
 var Blog = mongoose.model("Blog",blogSchema);
+
+//Creating sample data
+// Blog.create({
+//     title: "Test Blog",
+//     image: "https://bit.ly/2MgRARF",
+//     body: "First Blog Post"
+// });
+
+//RESTful Routes
+app.get("/blogs",function(req,res){
+    res.render("index.ejs");
+});
 
 //Start Server
 app.listen(5500,function(){
