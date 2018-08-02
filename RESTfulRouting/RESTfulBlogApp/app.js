c76ebd96ad1e8be3530f9ddffc39c1e9b6f80e88
Rion5 <chaigrindean@gmail.com>
2018-08-02 16:18:53 -0400

    Update app.js: Connected mongoose to DB

--- RESTfulRouting/RESTfulBlogApp/app.js
+++ RESTfulRouting/RESTfulBlogApp/app.js
@@ -2,4 +2,5 @@ var express     = require("express"),
     mongoose    = require("mongoose"),
     bodyParser  = require("body-parser"),
     app         = express();
-    
+
+mongoose.connect("mongodb://localhost:27017/restful_blog_app");
