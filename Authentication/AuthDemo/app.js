e75f3596978fd8f7456140cbe0776930184b306a
Rion5 <chaigrindean@gmail.com>
2018-08-13 15:00:52 -0400

    Update required packages, and connect mongoDB

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,7 +1,12 @@
-var express     =   require("express"),
-    bodyParser  =   require("body-parser"), 
-    app         =   express();
+var express                 = require("express"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    bodyParser              = require("body-parser"),
+    LocalStrategy           = require("passport-local"),
+    passportLocalMongoose   = require("passport-local-mongoose"),
+    app                     = express();
 
+mongoose.connect("mongodb://localhost:27017//auth_demo", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs"); 
 
