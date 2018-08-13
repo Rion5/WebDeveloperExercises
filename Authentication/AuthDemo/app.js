513769cbfbd5032c294373818596465dac24a780
Rion5 <chaigrindean@gmail.com>
2018-08-13 14:49:56 -0400

    Add app.get (Home page & error page) and app.listen to start server

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,5 +1,20 @@
-var express =   require("express"),
-    app     =   express();
+var express     =   require("express"),
+    bodyParser  =   require("body-parser"), 
+    app         =   express();
 
-app.use(bodyParser.urlencoded({extended: true})); 
-app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs"); 
+
+//Home Page
+app.get("/", function(req,res){
+    res.render("home.ejs");
+});
+//Error Page
+app.get("/*", function(req,res){
+    res.send("Error: Could not load page");
+});
+//Start Server
+app.listen(5500, function(){
+    console.log("Auth Demo Server has Started");
+    console.log("Go to 'localhost:5500'");
+});
