2115420c4f45f8b81bc25c1c607a8c6201df3969
Rion5 <chaigrindean@gmail.com>
2018-08-13 15:05:24 -0400

    Add User Model

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -2,6 +2,7 @@ var express                 = require("express"),
     mongoose                = require("mongoose"),
     passport                = require("passport"),
     bodyParser              = require("body-parser"),
+    User                    = require("./models/user"),
     LocalStrategy           = require("passport-local"),
     passportLocalMongoose   = require("passport-local-mongoose"),
     app                     = express();
