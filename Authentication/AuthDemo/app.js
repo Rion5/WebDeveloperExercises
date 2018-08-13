8cdbcc85cc6f52cbad3c416f2d820c4e761b769c
Rion5 <chaigrindean@gmail.com>
2018-08-13 15:25:00 -0400

    Add passport configuration setup

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -7,9 +7,20 @@ var express                 = require("express"),
     passportLocalMongoose   = require("passport-local-mongoose"),
     app                     = express();
 
-mongoose.connect("mongodb://localhost:27017//auth_demo", {useNewUrlParser: true});
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect("mongodb://localhost:27017/auth_demo", {useNewUrlParser: true});
 app.set("view engine", "ejs"); 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "Hidden Secret Message, Text could be anything",
+    resave: false,
+    saveUninitialized: false
+}));
+//We need the following 2 lines below, anytime we want to use passport
+app.use(passport.initialize());
+app.use(passport.session());
+//Responsible for reading the sessions
+passport.serializeUser(User.serializeUser());       // Encoding it (serialize) and putting it back into the session
+passport.deserializeUser(User.deserializeUser());   // Taking the data thats encoded in the sessions and un encoding it (deserialize)
 
 //Home Page
 app.get("/", function(req,res){
--- Authentication/AuthDemo/models/user.js
+++ Authentication/AuthDemo/models/user.js
@@ -1,8 +1,11 @@
 var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 var userSchema = new mongoose.Schema({
     userName: String,
     password: String
 });
+
+userSchema.plugin(passportLocalMongoose); //Takes our 'passport-local-mongoose', and adds a bunch of methods that come with the package to the userSchema
 
 module.exports = mongoose.model("User", userSchema);
