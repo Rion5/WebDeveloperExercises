b4fcc9aac1e970970038edda909e6313e9da3b16
Rion5 <chaigrindean@gmail.com>
2018-08-14 10:20:46 -0400

    Add passportLocalMongoose methods to User model

--- YelpCamp/models/user.js
+++ YelpCamp/models/user.js
@@ -1,9 +1,12 @@
 var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
 //Model Schema
 var userSchema = new mongoose.Schema({
     username: String,
     password: String
 });
+//Inject passport methods to User Model
+userSchema.plugin(passportLocalMongoose);
 //Compile into a model
 var User = mongoose.model("User", userSchema);
 //Return User
