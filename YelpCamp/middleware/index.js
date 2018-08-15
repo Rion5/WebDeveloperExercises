fa37b6b7a7f1fb3e423135709c04ae852916f137
Rion5 <chaigrindean@gmail.com>
2018-08-15 15:16:57 -0400

    Add middleware/index.js Keeping code DRY, extracting middleware code into this single file

---
+++ YelpCamp/middleware/index.js
@@ -0,0 +1,56 @@
+//========================
+// MIDDLEWARE
+//========================
+//Create an empty middlewareObj
+var middlewareObj = {};
+//Add middleware functions as object properties
+middlewareObj.checkCampgroundOwnership = function(req, res, next){
+    //1) Is user logged in?
+    if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, foundCampground){
+            if(err){
+                res.redirect("back");
+            } else{
+                //2) Does the user match the campground's author? (person who created the campground)
+                //     if true, then continue to call the next(); function
+                if(foundCampground.author.id.equals(req.user._id)){
+                   next();
+                } else{
+                    res.redirect("back");
+                }
+            }
+        });
+    } else{
+        //3) If not, redirect User back to previous page
+        res.redirect("back");   
+    }
+};
+middlewareObj.checkCommentOwnership = function(req, res, next){
+    //1) Is user logged in?
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect("back");
+            } else{
+                //2) Does the user match the comments's author?
+                //     if true, then continue to call the next(); function
+                if(foundComment.author.id.equals(req.user._id)){
+                   next();
+                } else{
+                    res.redirect("back");
+                }
+            }
+        });
+    } else{
+        //3) If not, redirect User back to previous page
+        res.redirect("back");   
+    }
+};
+middlewareObj.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+};
+
+module.exports = middlewareObj;
