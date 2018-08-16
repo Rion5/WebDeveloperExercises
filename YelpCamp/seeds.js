6cdb57788b6dcdf6090a5081f1cf14f20a47483b
Rion5 <chaigrindean@gmail.com>
2018-08-16 12:38:01 -0400

    Update: Refactor seeds.js Removed callback hell and using async / await

--- YelpCamp/seeds.js
+++ YelpCamp/seeds.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var Campground = require("./models/campground");
 var Comment   = require("./models/comment");
  
-var data = [
+var seeds = [
     {
         name: "Cloud's Rest", 
         image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
@@ -22,51 +22,35 @@ var data = [
     {
         name: "Canyon Floor", 
         image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        author: {
+            username: "Marge"
+        }
     }
 ];
  
-function seedDB(){
-   //Remove all campgrounds
-   Campground.remove({}, function(err){
-        if(err){
-            console.log(err);
-        }
-        console.log("Removed campgrounds!");
-        Comment.remove({}, function(err) {
-            if(err){
-                console.log(err);
+async function seedDB(){
+   //Remove all campgrounds and comments
+   await Campground.remove({});
+   console.log("Campgrounds removed");
+   await Comment.remove({});
+   console.log("Comments removed");
+   //Foreach seed, inside the seeds array
+   for(const seed of seeds){
+       let campgroud = await Campground.create(seed);
+       console.log("Campgrounds created");
+       let comment = await Comment.create(
+        {
+            text: "This place is great, but I wish there was internet",
+            author: {
+                username: "Homer"
             }
-            console.log("Removed comments!");
-            //Add a few campgrounds
-            data.forEach(function(seed){
-                Campground.create(seed, function(err, campground){
-                    if(err){
-                        console.log(err);
-                    } else {
-                        console.log("Added a campground");
-                        //Create a comment
-                        Comment.create(
-                            {
-                                text: "This place is great, but I wish there was internet",
-                                author: {
-                                    username: "Homer"
-                                }
-                            }, function(err, comment){
-                                if(err){
-                                    console.log(err);
-                                } else {
-                                    campground.comments.push(comment);
-                                    campground.save();
-                                    console.log("Created new comment");
-                                }
-                            });
-                    }
-                });
-            });
         });
-    }); 
-    //add a few comments
+        console.log("Comment created");
+        campgroud.comments.push(comment);
+        campgroud.save();
+        console.log("Comment added to campground");
+    }
 }
  
 module.exports = seedDB;
