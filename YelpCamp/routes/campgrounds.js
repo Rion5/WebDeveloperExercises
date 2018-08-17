e7733fa8a7efbed6c492401095d244e43a49dfc1
Rion5 <chaigrindean@gmail.com>
2018-08-17 10:37:17 -0400

    Update flash error message

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -44,7 +44,7 @@ router.post("/", middleware.isLoggedIn,function(req,res){
     };
     geocoder.geocode(req.body.location, function(err, data){
         if(err || !data.length){
-            req.flash("error", "Invalid Address");
+            req.flash("error", err.message);
             return res.redirect("back");
         }
         var lat = data[0].latitude;
