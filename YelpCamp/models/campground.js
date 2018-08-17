8edacf5228e9c4f6448d655aa4a30b961ac4876b
Rion5 <chaigrindean@gmail.com>
2018-08-17 11:34:06 -0400

    Add createdAt property to Campground and Comment Model Schema

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -7,6 +7,10 @@ var campgroundSchema = new mongoose.Schema({
     location: String,
     lat: Number,
     lng: Number,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
     author: {
         id: {
             type: mongoose.Schema.Types.ObjectId,
